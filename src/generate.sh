bf5f990daf45fd8370da66a156df0443cb58abb2
B06902101 <b06902101@csie.ntu.edu.tw>
2019-06-10 21:41:06 +0800

    Prototype generate.sh

---
+++ src/generate.sh
@@ -0,0 +1,12 @@
+if [[ $1 = f ]]
+	func = f
+else 
+	func = m
+fi
+
+addr=127.0.0.1
+
+for input_file in *.in; do
+	sudo ./user_program/master $input_file $func &
+		sudo ./user_program/slave $input_file.out $func addr
+done;
