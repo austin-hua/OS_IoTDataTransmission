eac14f43e324ad6fa2b835501ab0b7fb0d76e8da
B06902101 <b06902101@csie.ntu.edu.tw>
2019-06-10 19:16:54 +0800

    Preliminary mmap

--- src/user_program/master.c
+++ src/user_program/master.c
@@ -11,59 +11,88 @@
 
 #define PAGE_SIZE 4096
 #define BUF_SIZE 512
-size_t get_filesize(const char* filename);//get the size of the input file
 
+size_t get_filesize(const char* filename);//get input file size
 
 int main (int argc, char* argv[])
 {
 	char buf[BUF_SIZE];
 	int i, dev_fd, file_fd;// the fd for the device and the fd for the input file
-	size_t ret, file_size, offset = 0, tmp;
+	size_t ret, file_size, tmp, offset = 0;
 	char file_name[50], method[20];
 	char *kernel_address = NULL, *file_address = NULL;
 	struct timeval start;
 	struct timeval end;
 	double trans_time; //calulate the time between the device is opened and it is closed
-
+	void *our_map;
 
 	strcpy(file_name, argv[1]);
 	strcpy(method, argv[2]);
 
 
-	if( (dev_fd = open("/dev/master_device", O_RDWR)) < 0)
-	{
-		perror("failed to open /dev/master_device\n");
-		return 1;
-	}
-	gettimeofday(&start ,NULL);
-	if( (file_fd = open (file_name, O_RDWR)) < 0 )
-	{
-		perror("failed to open input file\n");
+	if((dev_fd = open("/dev/master_device", O_RDWR)) < 0) {
+		perror("Error opening /dev/master_device\n");
 		return 1;
 	}
 
-	if( (file_size = get_filesize(file_name)) < 0)
-	{
+	gettimeofday(&start ,NULL);
+
+	if((file_fd = open(file_name, O_RDWR)) < 0) {
+		perror("Error opening input file\n");
+		return 1;
+	}
+
+	if((file_size = get_filesize(file_name)) < 0) {
 		perror("failed to get filesize\n");
 		return 1;
 	}
 
-
-	if(ioctl(dev_fd, 0x12345677) == -1) //0x12345677 : create socket and accept the connection from the slave
-	{
-		perror("ioclt server create socket error\n");
+	//0x12345677 : create socket and accept the connection from the slave
+	if(ioctl(dev_fd, 0x12345677) == -1) {
+		perror("ioclt server socket creation error\n");
 		return 1;
 	}
 
+	void *mmapper;
 
-	switch(method[0])
-	{
-		case 'f': //fcntl : read()/write()
-			do
-			{
+	switch(method[0]) {
+		case 'f': //file I/O
+			do {
 				ret = read(file_fd, buf, sizeof(buf)); // read from the input file
-				write(dev_fd, buf, ret);//write to the the device
-			}while(ret > 0);
+				write(dev_fd, buf, ret); //write to the the device
+			} while(ret > 0);
+			break;
+
+		default: //mmap
+			//void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
+			//can use map_shared or map_private
+			mmapper = mmap(NULL, file_size, PROT_READ, MAP_SHARED, file_fd, 0);
+			/*
+			On success, mmap() returns a pointer to the mapped area.  On error,
+	       	the value MAP_FAILED (that is, (void *) -1) is returned, and errno is
+	       	set to indicate the cause of the error.
+	       	*/
+			if(mmapper == MAP_FAILED) {
+				perror("mmap creation failed\n");
+				return -1;
+			}
+
+			ret = file_size; //?
+
+			do {
+				if(ret >= 512) {
+					
+				}
+			}
+
+
+
+
+			//int munmap(void *addr, size_t length);
+			if(munmap(mmapper, file_size) == -1) {
+				perror("unmapping failed\n");
+				return -1;
+			}
 			break;
 	}
 
