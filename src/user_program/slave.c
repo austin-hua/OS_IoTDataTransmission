23b47cc7b7ea152ed63cb80d75ff114e1762a9ba
B06902101 <b06902101@csie.ntu.edu.tw>
2019-06-11 19:12:24 +0800

    Update slave.c

--- src/user_program/slave.c
+++ src/user_program/slave.c
@@ -15,7 +15,7 @@
 #define slave_IOCTL_MMAP 0x12345678
 #define slave_IOCTL_EXIT 0x12345679
 
-const size_t PAGE_SIZE = 4096 //4 kb
+const size_t PAGE_SIZE = 4096; //4 kb
 
 int main (int argc, char* argv[])
 {
